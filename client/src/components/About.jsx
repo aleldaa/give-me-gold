6fb8fd96cf13b79b4fa187e875390d38d94408bd
Luna <aleldaproductions@gmail.com>
2023-08-18 16:06:01 -0500

    working on about page

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -10,10 +10,30 @@ function About(){
                     <div className="banner-container">
                         <img className="banner" src="/src/images/give-me-gold-banner.png" />
                     </div>
-                    <div>
+                    <div className="terri">
                         <img className="terri-pic" src="/src/images/givemegold-3.jpg" />
                         <h3 className="terri-title">Terri</h3>
                         <p className="terri-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div className="torryn">
+                        <img className="torryn-pic" src="/src/images/givemegold-2.jpg" />
+                        <h3 className="torryn-title">Torryn</h3>
+                        <p className="torryn-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div className="lyus">
+                        <img className="lyus-pic" src="/src/images/lyus-1.png" />
+                        <h3 className="lyus-title">Lyus</h3>
+                        <p className="lyus-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div className="em">
+                        <img className="em-pic" src="/src/images/givemegold-8.jpg" />
+                        <h3 className="em-title">Em</h3>
+                        <p className="em-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    </div>
+                    <div className="ellie">
+                        <img className="ellie-pic" src="/src/images/ellie-1.jpg" />
+                        <h3 className="ellie-title">Ellie</h3>
+                        <p className="ellie-bio">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                     </div>
                 </div>
             </div>
