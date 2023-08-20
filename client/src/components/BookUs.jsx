913af08532d027591dbb7447118d7bf0cd89066e
Luna <aleldaproductions@gmail.com>
2023-08-20 09:25:18 -0500

    started on the book us contact form

--- client/src/components/BookUs.jsx
+++ client/src/components/BookUs.jsx
@@ -1,9 +1,13 @@
+import ContactForm from "./ContactForm"
 import NavBar2 from "./NavBar2"
 
 function BookUs(){
     return(
         <div>
             <NavBar2 />
+            <div>
+                <ContactForm/>
+            </div>
         </div>
     )
 }
